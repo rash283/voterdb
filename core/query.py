1b60b1565e0d71ddf75974964149af124536063a
Richard <richard.rash283@gmail.com>
2018-04-27 07:42:52 -0400

    creates database tables from fix width files

--- core/query.py
+++ core/query.py
@@ -1,11 +1,12 @@
 from core.database import engine
-from core.tables import voters, mailing_in_process, residence_in_process, oldham_roads, voting_history, mailing_std
+from core.tables import voters, mailing_in_process, oldham_roads, voting_history, mailing_std, tiger_zip_state_loc, \
+    tiger_addr, tiger_addrfeat
 from core.funcs import load_reference_street
 from sqlalchemy import select, alias, text, and_, bindparam
 from sqlalchemy.sql import text
 from sqlalchemy.sql import func
 import pandas as pd
-
+import numpy as np
 
 
 def geocode():
@@ -22,20 +23,52 @@ def distinct_streets_oldham():
     return streets
 
 
+def get_addressee(group):
+    group['occupant'] = len(group)
+    if (group.sex == 'M').any():
+        oldest = group.yob.idxmin()
+        oldest_male = group.loc[group.sex == 'M', 'yob'].idxmin()
+        if oldest == oldest_male:
+            addressee = group.loc[oldest]
+        else:
+            oldest_yob = group.loc[oldest, 'yob']
+            oldest_male_yob = group.loc[oldest_male, 'yob']
+            if (oldest_yob - oldest_male_yob) / np.timedelta64(365, 'D') > 16:
+                addressee = group.loc[oldest]
+            else:
+                addressee = group.loc[oldest_male]
+    else:
+        oldest = group.yob.idxmin()
+        addressee = group.loc[oldest]
+    return addressee
+
+
 def get_mailing_addresses_rep_prim():
     j = voters.join(mailing_in_process)
     j1 = voters.join(mailing_std)
-    stmt = select([mailing_in_process]).select_from(j).where(
+    stmt = select([mailing_in_process, voters.c.PrecinctCode, voters.c.Sex,
+                   voters.c.DateOfBirth, voters.c.primary_sum]).select_from(j).where(
         and_(voters.c.Party == 'R', voters.c.primary_sum >= 2))
     stmt1 = select([mailing_std]).select_from(j1).where(
         and_(voters.c.Party == 'R', voters.c.primary_sum >= 2))
     with engine.connect() as con:
         results = con.execute(stmt).fetchall()
         results1 = con.execute(stmt1).fetchall()
-    df = pd.DataFrame(results, columns=['id', 'first_name', 'last_name', 'street', 'city', 'state', 'zip', 'macro'])
+    df = pd.DataFrame(results, columns=['id', 'first_name', 'last_name', 'street', 'city',
+                                        'state', 'zip', 'macro', 'precinct', 'sex', 'yob', 'p_sum'])
     df.index = df['id']
     del(df['id'])
+    df.yob = pd.to_datetime(df.yob)
     df['street'] = df['street'].str.replace(r'P\.?\s?O\.?\s?BOX\s+(\d+)', r'P.O. BOX \1')
+    df['street'] = df['street'].str.replace(r'\s+', r' ')
+    df['zip'] = df['zip'].str.slice(0, 5)
+    for col in df.columns:
+        if df[col].dtype == np.object_:
+            df[col] = df[col].str.strip()
+    df = df.assign(
+        address=lambda x: (x['street'] + ', ' + x['city'] + ', ' + x['state'] + ', ' + x['zip']))
+    pattern = r' I{2,3}| JR| SR| IV'
+    df['last_name_stripped'] = df.last_name.str.rstrip(pattern)
     df1 = pd.DataFrame(results1, columns=results1[0].keys())
     df1.index = df1['index']
     del(df1['index'])
@@ -45,17 +78,13 @@ def get_mailing_addresses_rep_prim():
     df2 = pd.merge(df, df1, left_index=True, right_index=True)
     df2.loc[df2['street'] == '0', 'street'] = None
     df2 = df2.dropna(subset=['street'])
-    # I left off here
-    df2['address'] = ''
-    df2['street1'] = ''
-    parts = ['pre', 'streetname', 'streettype', 'post']
-    for row in df2.itertuples():
-        rowdict = row._asdict()
-        df2.loc[row.Index, 'address'] = f'{row.street}, {row.city}, {row.state}, {row.zip[:5]}'
-        df2.loc[row.Index, 'street1'] = ' '.join([str(rowdict[part]) for part in parts if rowdict[part]])
-    pattern = r' I{2,3}| JR| SR| IV'
-    df['last_name_stripped'] = df.last_name.str.rstrip(pattern)
+    df2.loc[df2.state_y == 'KENTUCKY', 'state_y'] = 'KY'
+    pg = df2.groupby('precinct')
 
+    for name, group1 in pg.groups:
+        addys = group1.groupby(['house_num', 'name'])
+        for (hn, street), group in addys:
+            print(get_addressee(group))
     with open('mailing_addys.csv', 'w') as file:
         for addy in df.address.unique():
             family = df[df.address == addy]
@@ -102,3 +131,44 @@ def update_voter_score():
         .values(general_sum=bindparam('general_sum'))
     with engine.connect() as con:
         results = con.execute(stmt, gs)
+
+
+def get_ky_zips():
+    stmt = select([tiger_zip_state_loc.c.zip, tiger_zip_state_loc.c.place]) \
+        .where(tiger_zip_state_loc.c.statefp == '21')
+    with engine.connect() as con:
+        results = con.execute(stmt).fetchall()
+    zips = pd.DataFrame(results, columns=results[0].keys())
+    return zips
+
+
+def compare_city_zip(df, zip_df):
+        z = df.city.unique()
+        for i in z:
+            for city in df.loc[df.zip5 == i, 'city'].unique():
+                zip_cities = zip_df.loc[zip_df.zip == i, 'place'].values
+                if city not in zip_cities:
+                    print(f'Zip code:{i}, listed city: {city}')
+                    print(zip_cities)
+
+
+def get_oldham_zips():
+    j = tiger_addr.join(tiger_addrfeat)
+
+
+    a101 = pg.get_group('A103')
+    a101_addys = a101.groupby(['house_num', 'name'])
+    addresses = [get_addressee(group) for (hn, street), group in a101_addys]
+
+
+
+        # if len(group.address.unique()) > 1:
+        #     men = len(group[group.sex == 'M'])
+        #     if men == 1:
+        #
+        #         print(group.loc[group.sex == 'M', 'address'].values[0])
+        #     elif men > 1:
+        #         group.address = group[group.index == group.loc[group.sex == 'M', 'yob'].idxmin()].address
+        #     else:
+        #         group.address = group[group.index == group.loc[:, 'yob'].idxmin()].address
+        #     print(f'Group:{hn}, {street}\nUnique addresses: {len(group.address.unique())}\n')
